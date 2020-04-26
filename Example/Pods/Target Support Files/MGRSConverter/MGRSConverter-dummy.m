e1a74f6c9f481ce7697722ea9402314a3f9cf36b
Kevin Kieffer <kdkieffer@icloud.com>
2020-04-26 15:26:30 -0400

    Updated from cocoapod     <true/>

---
+++ Example/Pods/Target Support Files/MGRSConverter/MGRSConverter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MGRSConverter : NSObject
+@end
+@implementation PodsDummy_MGRSConverter
+@end
---
+++ Example/Pods/Target Support Files/Pods-MGRSConverter_Example/Pods-MGRSConverter_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MGRSConverter_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MGRSConverter_Example
+@end
